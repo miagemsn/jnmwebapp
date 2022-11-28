9b0976ac366fe618e748b57267e1418b0bcabdae
monicatevy <monicatevy.sen@pm.me>
2022-11-28 11:29:09 +0100

    (admin) onglet partenaires, get all sponsors

---
+++ src/Controller/admin/AdminSponsorsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\admin;
+
+use App\Repository\SponsorRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminSponsorsController extends AbstractController
+{
+    /**
+     *
+     * @var SponsorRepository
+     */
+    private $repository;
+
+    /**
+     *
+     * @var EntityManagerInterface
+     */
+    private $om;
+
+    /**
+     *
+     * @param SponsorRepository $repository
+     * @param EntityManagerInterface $om
+     */
+    function __construct(SponsorRepository $repository, EntityManagerInterface $om) {
+        $this->repository = $repository;
+        $this->om = $om;
+    }
+
+    #[Route('/admin/partenaires', name: 'admin.sponsors')]
+    public function index(): Response
+    {
+        $sponsors = $this->repository->findAll();
+        return $this->render('admin/admin.sponsors.html.twig', [
+            'sponsors' => $sponsors,
+        ]);
+    }
+}
